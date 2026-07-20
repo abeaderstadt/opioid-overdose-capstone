68cab468d6b4db9cdb69932ea4dd397a02cf1dda
abeaderstadt <alissabeaderstadt@outlook.com>
2026-07-20 15:55:36 -0500

    Work on predictive modeling notebook

--- src/modeling/linear_regression.py
+++
@@ -1,3 +0,0 @@
-# This file is used to perform linear regression analysis on the dataset.
-# It includes functions to fit a linear regression model, evaluate its performance,
-# and make predictions based on the model.
--- src/modeling/random_forest.py
+++
@@ -1,3 +0,0 @@
-# This file is used to perform random forest analysis on the dataset.
-# It includes functions to fit a random forest model, evaluate its performance,
-# and make predictions based on the model.
